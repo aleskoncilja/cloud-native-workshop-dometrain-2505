ed1553253a2431f048079c422eb914a198384c39
Elfocrash <nick.chapsas@gmail.com>
2025-05-14 11:34:14 +0100

    latest

--- src/Dometrain.Aspire.AppHost/Program.cs
+++ src/Dometrain.Aspire.AppHost/Program.cs
@@ -1,6 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Dometrain_Monolith_Api>("dometrain-api");
+var mainDbUsername = builder.AddParameter("postgres-username");
+var mainDbPassword = builder.AddParameter("postgres-password");
+
+var mainDb = builder.AddPostgres("main-db", mainDbUsername, mainDbPassword, port: 5432)
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume()
+    .AddDatabase("dometrain");
+
+builder.AddProject<Projects.Dometrain_Monolith_Api>("dometrain-api")
+    .WithReference(mainDb);
 
 var app = builder.Build();
     
